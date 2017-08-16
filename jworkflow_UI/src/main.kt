563917c463aa77de3d693d67df7e2bd4b48e5797
sanstorik <shvetsovvitaliy06@gmail.com>
2017-08-16 15:40:17 +0300

    updated main func

--- jworkflow_UI/src/main.kt
+++ jworkflow_UI/src/main.kt
@@ -1,11 +1,28 @@
 import activities.days.Days
 import activities.projects.Projects
 import native_hooks.ActivityGlobalListener
+import panels.WorkflowTabbedPane
 import java.util.*
+import javax.swing.JFrame
+import javax.swing.SwingUtilities
+import javax.swing.UIManager
 import kotlin.concurrent.timerTask
 
-fun main(args: Array<String>) {
-    ActivityGlobalListener.of(Days.currentDay).startTracking()
+val FRAME_WIDTH = 750
+val FRAME_HEIGHT = 450
 
-    Timer().schedule(timerTask { Days.saveChanges()}, 5000)
+fun main(args: Array<String>) {
+    SwingUtilities.invokeLater {
+        UIManager.put("swing.boldMetal", false)
+        createAndShowGui()
+    }
+}
+
+private fun createAndShowGui() {
+    val frame = JFrame()
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    frame.add(WorkflowTabbedPane())
+
+    frame.setSize(FRAME_WIDTH, FRAME_HEIGHT)
+    frame.isVisible = true
 }
